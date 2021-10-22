4a6149f0afcefded227dfcfdbf166d8591b41c77
Abeer Ali Khan <aalkhan@students.uit.edu>
2021-10-22 20:25:57 +0500

    add another dice

--- lesson-1-building-first-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ lesson-1-building-first-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,19 +9,27 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     // We're using lateinit here because we will initialize it late and promising the compiler it will be initialized
     // before any operation will be performed on it
-    lateinit var diceImageContainer: ImageView
+    lateinit var diceImageContainer1: ImageView
+    lateinit var diceImageContainer2: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // made a reference object to the button by using the given id to the button "roll_button"
         val roll_button: Button = findViewById(R.id.roll_button)
-        diceImageContainer = findViewById(R.id.dice_image)
+        diceImageContainer1 = findViewById(R.id.dice_image1)
+        diceImageContainer2 = findViewById(R.id.dice_image2)
         roll_button.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val imageToShow = when(randomInt) {
+        diceImageContainer1.setImageResource(getRandomDice())
+        diceImageContainer2.setImageResource(getRandomDice())
+        Toast.makeText(this, "Dices have been rolled!",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getRandomDice(): Int {
+        return when((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -29,8 +37,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImageContainer.setImageResource(imageToShow)
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
     }
 }
